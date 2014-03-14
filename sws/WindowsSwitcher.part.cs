994754677e80519c9eea6998b21e7dfe8fc07373
Cignoir <cignoir@gmail.com>
2014-03-14 19:25:26 +0900

    Divided WindowsSwitcher class to exclude win32 api related.

--- sws/WindowsSwitcher.part.cs
+++ sws/WindowsSwitcher.part.cs
@@ -15,28 +15,11 @@ namespace sws
 {
     public partial class WindowsSwitcher: Form
     {
-        const int MOD_ALT = 0x0001;
-        const int MOD_CONTROL = 0x0002;
-        const int MOD_SHIFT = 0x0004;
-        const int MOD_WIN = 0x0008;
-        const int WM_HOTKEY = 0x0312;
-        const int WM_HOTKEY1 = 0x0001;
-        const int WM_HOTKEY2 = 0x0002;
-        const int WM_HOTKEY3 = 0x0003;
-        const int WM_HOTKEY4 = 0x0004;
-        const int WM_HOTKEY5 = 0x0005;
-
         public WindowsSwitcher()
         {
             InitializeComponent();
             ShowAllProcesses();
         }
-
-        [DllImport("user32.dll")]
-        extern static int RegisterHotKey(IntPtr HWnd, int ID, int MOD_KEY, int KEY);
-
-        [DllImport("user32.dll")]
-        extern static int UnregisterHotKey(IntPtr HWnd, int ID);
 
         private void ActivateWindow(IntPtr hWnd)
         {
@@ -79,81 +62,6 @@ namespace sws
                 AttachThreadInput(thisThread, foreThread, false);
             }
         }
-
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool SetForegroundWindow(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetForegroundWindow();
-
-        [DllImport("user32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool BringWindowToTop(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        static extern IntPtr SetFocus(IntPtr hWnd);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool SetWindowPos(IntPtr hWnd,
-            int hWndInsertAfter, int x, int y, int cx, int cy, int uFlags);
-
-        private const int SWP_NOSIZE = 0x0001;
-        private const int SWP_NOMOVE = 0x0002;
-        private const int SWP_NOZORDER = 0x0004;
-        private const int SWP_SHOWWINDOW = 0x0040;
-        private const int SWP_ASYNCWINDOWPOS = 0x4000;
-        private const int HWND_TOP = 0;
-        private const int HWND_BOTTOM = 1;
-        private const int HWND_TOPMOST = -1;
-        private const int HWND_NOTOPMOST = -2;
-
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
-
-        private const int SW_SHOWNORMAL = 1;
-        private const int SW_SHOWMINIMIZED = 2;
-        private const int SW_SHOW = 5;
-        private const int SW_RESTORE = 9;
-        private const int SW_FORCEMINIMIZE = 11;
-
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool IsIconic(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        private static extern uint GetWindowThreadProcessId(
-            IntPtr hWnd, IntPtr ProcessId);
-
-        [DllImport("kernel32.dll")]
-        private static extern uint GetCurrentThreadId();
-
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool AttachThreadInput(
-            uint idAttach, uint idAttachTo, bool fAttach);
-
-        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo",
-            SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool SystemParametersInfoGet(
-            uint action, uint param, ref uint vparam, uint init);
-
-        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo",
-            SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool SystemParametersInfoSet(
-            uint action, uint param, uint vparam, uint init);
-
-        private const uint SPI_GETFOREGROUNDLOCKTIMEOUT = 0x2000;
-        private const uint SPI_SETFOREGROUNDLOCKTIMEOUT = 0x2001;
-        private const uint SPIF_UPDATEINIFILE = 0x01;
-        private const uint SPIF_SENDCHANGE = 0x02;
 
         private void FilterListBox()
         {
